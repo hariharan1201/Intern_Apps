78998e8af8594de19c47c34400c83e7eae1dd8f2
HariharanM <hariakash1117@gmail.com>
2023-03-09 14:48:11 +0530

    Homepage

---
+++ lib/home.dart
@@ -0,0 +1,72 @@
+import 'package:app_2/PickPage.dart';
+import 'package:flutter/material.dart';
+
+// ignore: non_constant_identifier_names
+
+class output extends StatefulWidget {
+  output({super.key});
+  // ignore: non_constant_identifier_names
+  String fname = "";
+  f_name(String name) {
+    fname = name;
+    print(fname);
+  }
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+}
+
+class homepage extends StatefulWidget {
+  const homepage({super.key});
+
+  @override
+  State<homepage> createState() => _homepageState();
+}
+
+class _homepageState extends State<homepage> {
+  output o = new output();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            InkWell(
+              child: Container(
+                height: 100,
+                width: 450,
+                color: Colors.amber,
+              ),
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => const pickpage()));
+              },
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+              height: 100,
+              width: 450,
+              color: Colors.amber,
+              // ignore: unnecessary_this
+              child: Center(
+                child: Text(
+                  ("${o.fname}"),
+                  style: const TextStyle(
+                      fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+          ],
+        ),
+      )),
+    );
+  }
+}
