d2c2e4bddea84c0e644c61eafc6496285d36d679
HariharanM <hariakash1117@gmail.com>
2023-03-09 14:48:31 +0530

    Main

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:app_2/PickPage.dart';
+import 'package:flutter/material.dart';
+
+import 'home.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: homepage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
